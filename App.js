9f85621e3dab3c2833b6d0a6f8ad7b96246a730a
GboyegaAkere <oluwagboyegaakeremale80@gmail.com>
2023-10-13 20:19:39 +0100

    new changes

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import AppNavigator from './AppNavigator'
+import { View } from 'react-native'
+
+const App = () => {
+  return (
+    
+       <AppNavigator/>
+   
+  )
+}
+
+export default App
