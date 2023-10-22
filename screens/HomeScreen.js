b424e691d127a2261679e0b11dfde51713b45c5f
GboyegaAkere <oluwagboyegaakeremale80@gmail.com>
2023-10-22 15:10:13 +0100

    working on the homeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,37 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar, ScrollView,Image, TextInput } from 'react-native'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import {BellIcon, MagnifyingGlassIcon} from 'react-native-heroicons/outline'
 
 const HomeScreen = () => {
+  
+  const networkImage = { uri: "https://i.ibb.co/nMdcxWw/avatar.jpg" };
+
   return (
-    <View>
-      <Text className="font-bold text-4xl">HomeScreen</Text>
+    <View className ="flex-1 bg-white">
+      <ScrollView 
+       contentContainerStyle={{paddingBottom: 50, padding:5}}
+       className="space-y-6 pt-6">
+      
+         <View className="flex flex-row items-center justify-between">
+           <Image source={networkImage} style={{height: hp(5), width: hp(5.5)}} className="rounded-full" />
+           <BellIcon size={hp(4)} color="gray"/>
+         </View>
+
+
+        <View className="">
+          <Text style={{fontSize:hp(2)}}>Hello,Gboyega!</Text>
+          <Text className="text-2xl font-semibold">Discover and Cook,</Text>
+          <Text style={{fontSize:hp(4)}} className="">amazing <Text className="text-yellow-500">dishes</Text> </Text>
+        </View>
+
+        <View className="flex flex-row justify-between p-2 bg-gray-200 rounded-full">
+          <TextInput className="flex-1 text-base tracking-wider mb-1 pl-3" style={{fontSize:hp(1.7)}} placeholder='search for favorite food'/>
+          <View className="bg-white rounded-full w-8 h-8 items-center justify-center">
+            <MagnifyingGlassIcon/>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   )
 }
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -3,25 +3,33 @@ import { StatusBar } from 'expo-status-bar'
 import { View, Text,Image } from 'react-native'
 import { widthPercentageToDP as wp , heightPercentageToDP as hp } from 'react-native-responsive-screen'
 import Animated, { useSharedValue, withSpring } from 'react-native-reanimated'
+import { useNavigation } from '@react-navigation/native'
  
 
 const WelcomeScreen = () => {
   const ringPadding1 = useSharedValue(0)
   const ringPadding2 = useSharedValue(0)
+  const navigation = useNavigation()
 
   React.useEffect (()=>{
-    // ringPadding1.value = 0
-    // ringPadding2.value = 0
+    ringPadding1.value = 0
+    ringPadding2.value = 0
 
     setTimeout(() => {
       ringPadding1.value = withSpring(ringPadding1.value+hp(5))
     },100);
 
     setTimeout(() => {
-      ringPadding2.value = withSpring(ringPadding2.value+hp(5))
-    },100);
+      ringPadding2.value = withSpring(ringPadding2.value+hp(5.5))
+    },300);
+
+    setTimeout(() => {
+      navigation.navigate("Home")
+    },2500);
 
   },[])
+
+  
   return (
     <View className="flex-1 justify-center items-center bg-green-300 space-y-10">
         <Animated.View className="rounded-full bg-white/20" style={{padding:ringPadding1}}>
